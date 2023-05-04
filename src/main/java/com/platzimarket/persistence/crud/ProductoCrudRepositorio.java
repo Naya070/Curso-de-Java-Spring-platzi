e4ec3a63923f95f5ca59766b60601e738bdc3bc0
Naya070 <nayarith070@gmail.com>
2023-05-04 16:36:54 -0400

    se realizaron Query Methods

--- src/main/java/com/platzimarket/persistence/crud/ProductoCrudRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.platzimarket.persistence.crud;
-
-import com.platzimarket.persistence.entity.Producto;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductoCrudRepositorio extends CrudRepository<Producto, Integer> {
-}
